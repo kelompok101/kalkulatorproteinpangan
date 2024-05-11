17f47b0a9dcc3be72e61be9a16714f6558613fe4
kelompok101 <167988498+kelompok101@users.noreply.github.com>
2024-05-11 23:49:28 +0700

    Update kalkulatorprotein.py

--- kalkulatorprotein.py
+++ kalkulatorprotein.py
@@ -135,6 +135,8 @@ elif selected == "Perkenalan Kelompok":
     st.title("🌟 Aplikasi Perhitungan Kadar Protein 🍏🥩")
     st.write("Selamat datang di aplikasi perhitungan kadar protein dalam produk pangan!")
     st.write("Kami hadir untuk membantu Anda menghitung kadar protein dalam berbagai jenis makanan.")
+    image_path = 'kerkom3.jpg'  # Ganti dengan path yang sesuai ke file gambar Anda
+    st.image(image_path, caption='Ilustrasi Kadar Protein dalam Berbagai Makanan')
     st.write("Dibuat oleh Kelompok 10 Kelas PMIP Mata Kuliah Logika dan Pemrograman Komputer.")
 
     # Garis pembatas berwarna-warni
